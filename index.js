6082124bdca65ea99688e9de076805812960aa78
diljitsingh2202 <161720221+diljitsingh2202@users.noreply.github.com>
2024-02-29 20:17:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let arr = ["1111111111","2222222222","3333333333","4444444444","5555555555"]
+let link = "https://wa.me/91"
+
+let button = document.getElementById("changing")
+
+function change(){
+    const randomElement = arr[Math.floor(Math.random() * arr.length)];
+    link = `https://wa.me/91${randomElement}`
+    button.href = link
+}
